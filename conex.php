4a5b89aa1737a41623c7426cb2caac265bdfc8e5
Robersillo <31243904+Robersillo@users.noreply.github.com>
2017-09-18 16:56:01 +0200

    COnex Modificado
    COnex Modificado, para el servidor WEB

--- conex.php
+++ conex.php
@@ -1,6 +1,6 @@
 <?php
-$link=mysqli_connect("localhost","root","");
-mysqli_select_db($link,"repuestos.ffventas");
+$link=mysqli_connect("localhost:3306","ffven_repuestos","r20697009");
+mysqli_select_db($link,"ffventa1_repuestos");
 if (mysqli_connect_errno())
 {
     echo "Failed to connect to mysqli: " . mysqli_connect_error();
